e8238b52cdb9a9a61b2081f81c4e3edb29355a7a
kidereo <kirill.derevenski@gmail.com>
2022-11-02 16:28:26 +0100

    Added line gradient on the session length chart.

--- src/components/dashboard/charts/SessionLengthChart.js
+++ src/components/dashboard/charts/SessionLengthChart.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer} from 'recharts';
 import SessionLength from "../../../resources/SessionLength";
 import * as PropTypes from 'prop-types';
@@ -12,13 +12,69 @@ import * as PropTypes from 'prop-types';
  * @constructor
  */
 export default function SessionLengthChart({title, userSessionLength}) {
+    /**
+     * Get data for the chart.
+     *
+     * @type {SessionLength}
+     * @type {*}
+     */
     let currentUserSessionLength = new SessionLength(userSessionLength);
+    let chartData = currentUserSessionLength.retrieveSessionLength;
+
+    /**
+     * A set of functions to animate the chart's container and the graph line on hover.
+     */
+
+    /**
+     * 1. Calculate percentage for the gradient colour
+     * so that the line to the right of hovered datapoint can remain white
+     * but the line to the left will become 50% opaque.
+     *
+     * 2. Reset line percentage back to 0.
+     *
+     * @param hoveredDataPoint
+     */
+    let [percentage, setPercentage] = useState(0);
+
+    function calculateLinePercentage(hoveredDataPoint) {
+        if (hoveredDataPoint && hoveredDataPoint.activePayload) {
+            let hoveredPoint = hoveredDataPoint.activePayload[0].payload.label;
+            let index = chartData.findIndex(item => item.label === hoveredPoint);
+            let variable = ((chartData.length - index - 1) * 100) / (chartData.length - 1);
+            setPercentage(100 - variable);
+        }
+    }
+
+    function resetLinePercentage() {
+        setPercentage(0);
+    }
+
+    /**
+     * 1. Set chart's background to gray to the right of hovered datapoint.
+     * 2. Reset chart's background back to red.
+     *
+     * @param event
+     */
+    function setBackground(event) {
+        if (event.isTooltipActive) {
+            let container = document.querySelector('.dashboard-graphs-charts-cards-session_length');
+            let containerWidth = container.clientWidth;
+            let mouseLocX = Math.round((event.activeCoordinate.x / containerWidth) * 100);
+            container.style.background = `linear-gradient(90deg, rgba(255,0,0,1) ${mouseLocX}%, rgba(225,0,0,1) ${mouseLocX}%, rgba(225,0,0,1) 100%)`;
+        }
+    }
+
+    function resetBackground() {
+        let container = document.querySelector('.dashboard-graphs-charts-cards-session_length');
+        container.style.background = "rgba(255, 0, 0, 1)";
+    }
+
     return (
         <div className="dashboard-graphs-charts-cards-session_length">
             <h2>{title}</h2>
             <ResponsiveContainer width="100%" height="70%">
                 <LineChart
-                    data={currentUserSessionLength.retrieveSessionLength}
+                    data={chartData}
                     margin={{
                         top: 5,
                         right: 25,
@@ -27,16 +83,12 @@ export default function SessionLengthChart({title, userSessionLength}) {
                     }}
                     outerRadius="75%"
                     onMouseMove={function (event) {
-                        if (event.isTooltipActive) {
-                            let container = document.querySelector('.dashboard-graphs-charts-cards-session_length');
-                            let containerWidth = container.clientWidth;
-                            let mouseLocX = Math.round((event.activeCoordinate.x / containerWidth) * 100);
-                            container.style.background = `linear-gradient(90deg, rgba(255,0,0,1) ${mouseLocX}%, rgba(225,0,0,1) ${mouseLocX}%, rgba(225,0,0,1) 100%)`;
-                        }
+                        setBackground(event);
                     }}
-                    onMouseOut={function () {
-                        let container = document.querySelector('.dashboard-graphs-charts-cards-session_length');
-                        container.style.background = "rgba(255, 0, 0, 1)";
+                    onMouseOver={calculateLinePercentage}
+                    onMouseLeave={function () {
+                        resetBackground();
+                        resetLinePercentage();
                     }}
                 >
                     <XAxis
@@ -67,7 +119,8 @@ export default function SessionLengthChart({title, userSessionLength}) {
                     <Line
                         type="natural"
                         dataKey="sessionLength"
-                        stroke="rgba(255, 255, 255, 0.5)"
+                        //stroke="rgba(255, 255, 255, 0.5)"
+                        stroke="url(#colourLine)"
                         strokeWidth={2}
                         dot={false}
                         activeDot={{
@@ -77,6 +130,14 @@ export default function SessionLengthChart({title, userSessionLength}) {
                             fill: "white"
                         }}
                     />
+                    <defs>
+                        <linearGradient id="colourLine" x1="0%" y1="0" x2="100%" y2="0">
+                            <stop offset="0%" stopColor="rgba(255, 255, 255, 0.5)"/>
+                            <stop offset={`${percentage}%`} stopColor="rgba(255, 255, 255, 0.4)"/>
+                            <stop offset={`${percentage}%`} stopColor="white"/>
+                            <stop offset={`${100}%`} stopColor="white"/>
+                        </linearGradient>
+                    </defs>
                 </LineChart>
             </ResponsiveContainer>
         </div>
